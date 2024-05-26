e32f49bde84d3d2be5605c174477847e4b06591b
thraal@gmail.com <thraal@gmail.com>
2024-05-26 11:58:21 +0200

    update

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -5,7 +5,7 @@ signal shoot_bullet(position: Vector2)
 var speed: float = 200.0
 
 func _ready() -> void:
-	collision_layer = 1  # Layer 1
+	collision_layer = 1  # Layer  1
 	collision_mask = 4   # Collide with layer 4 (enemies)
 
 func _input(event):
@@ -13,24 +13,24 @@ func _input(event):
 		# Rotate enemy
 		var target_position = get_viewport().get_mouse_position()
 		var direction = (target_position - position).normalized()
-		
+
 		self.rotation = atan2(direction.y, direction.x) + PI/2
-		
+
 		# Spawn bullet
 		emit_signal("shoot_bullet", self.get_node("ShootPoint").global_position)
 
 func _physics_process(delta: float) -> void:
 	var direction: Vector2 = Vector2.ZERO
-	
+
 	if Input.is_action_pressed("ui_right"):
 		direction.x += 1
 	if Input.is_action_pressed("ui_left"):
 		direction.x -= 1
-	
+
 	velocity.x = direction.x * speed
-	
+
 	move_and_slide()
-	
+
 	# Clamp the player's position within the viewport bounds
 	var viewport = get_viewport_rect()
 	position.x = clamp(position.x, 0, viewport.size.x)
